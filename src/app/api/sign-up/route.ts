07900ec6aada4c496baad478aa02fcb25d67a09a
Harshit-Raj-23 <harshitkashyap447@gmail.com>
2026-02-14 01:28:35 +0530

    Implemented Sign Up Route

---
+++ src/app/api/sign-up/route.ts
@@ -0,0 +1,95 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import bcrypt from "bcryptjs";
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+import { success } from "zod";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  try {
+    const { username, email, password } = await request.json();
+
+    const existingUserByUsername = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+    if (existingUserByUsername) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username is already taken!",
+        },
+        { status: 400 },
+      );
+    }
+
+    const hassedPassword = await bcrypt.hash(password, 10);
+    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+    const expiryDate = new Date();
+    expiryDate.setHours(expiryDate.getHours() + 1);
+
+    const existingUserByEmail = await UserModel.findOne({ email });
+    if (existingUserByEmail) {
+      if (existingUserByEmail.isVerified) {
+        return Response.json(
+          {
+            success: false,
+            message: "User already exists with this email!",
+          },
+          { status: 400 },
+        );
+      } else {
+        existingUserByEmail.password = hassedPassword;
+        existingUserByEmail.verifyCode = verifyCode;
+        existingUserByEmail.verifyCodeExpiry = expiryDate;
+        await existingUserByEmail.save();
+      }
+    } else {
+      const newUser = new UserModel({
+        username,
+        email,
+        password: hassedPassword,
+        verifyCode,
+        verifyCodeExpiry: expiryDate,
+        isVerified: false,
+        isAcceptingMessage: true,
+        messages: [],
+      });
+
+      await newUser.save();
+    }
+
+    const emailResponse = await sendVerificationEmail(
+      email,
+      username,
+      verifyCode,
+    );
+    if (!emailResponse.success) {
+      return Response.json(
+        {
+          success: false,
+          message: emailResponse.message,
+        },
+        { status: 500 },
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "User registered successfully! Please verify your account.",
+      },
+      { status: 201 },
+    );
+  } catch (error) {
+    console.error("Error registering user: ", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error registering user!",
+      },
+      { status: 500 },
+    );
+  }
+}
